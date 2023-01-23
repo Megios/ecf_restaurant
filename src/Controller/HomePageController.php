da626af12011a144861d9eaf9f3cac7f8773e010
megios <ad.messaoudene@gmail.com>
2023-01-23 23:30:49 +0100

    template footer1

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -11,8 +11,64 @@ class HomePageController extends AbstractController
     #[Route('/', name: 'app_home_page')]
     public function index(): Response
     {
+        $lundi = [
+            'open' => true,
+            'open_midi' => '12:00',
+            'close_midi' => '14:00',
+            'open_soir' => '17:00',
+            'close_soir' => '22:00'
+        ];
+        $mardi = [
+            'open' => true,
+            'open_midi' => '12:00',
+            'close_midi' => '14:00',
+            'open_soir' => '17:00',
+            'close_soir' => '22:00'
+        ];
+        $mercredi = [
+            'open' => false,
+        ];
+        $jeudi = [
+            'open' => true,
+            'open_midi' => '12:00',
+            'close_midi' => '14:00',
+            'open_soir' => '17:00',
+            'close_soir' => '22:00'
+        ];
+        $vendredi = [
+            'open' => true,
+            'open_midi' => '12:00',
+            'close_midi' => '14:00',
+            'open_soir' => '17:00',
+            'close_soir' => '22:00'
+        ];
+        $samedi = [
+            'open' => true,
+            'open_midi' => '',
+            'close_midi' => '',
+            'open_soir' => '17:00',
+            'close_soir' => '23:00'
+        ];
+
+        $dimanche = [
+            'open' => true,
+            'open_midi' => '12:00',
+            'close_midi' => '16:00',
+            'open_soir' => '',
+            'close_soir' => ''
+        ];
+        $semaine = [
+            'Lundi' => $lundi,
+            'Mardi' => $mardi,
+            'Mercredi' => $mercredi,
+            'Jeudi' => $jeudi,
+            'Vendredi' => $vendredi,
+            'Samedi' => $samedi,
+            'Dimanche' => $dimanche
+        ];
         return $this->render('home_page/index.html.twig', [
             'controller_name' => 'HomePageController',
+            'semaine' => $semaine
         ]);
     }
 }
